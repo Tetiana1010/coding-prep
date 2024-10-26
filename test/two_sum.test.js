9f51bd280153616f2e0fdd4185b29d116f1ccff0
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-10-26 21:24:14 +0300

    Basic setup

---
+++ test/two_sum.test.js
@@ -0,0 +1,9 @@
+import { twoSum } from '../tasks/algorithms/solutions/two_sum_solution';
+
+test('twoSum function exists', () => {
+  expect(typeof twoSum).toEqual('function');
+});
+
+test('Two Sum test case', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
